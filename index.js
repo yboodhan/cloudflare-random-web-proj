a6a8f221adfdc2a41f90e059e831c2ec18944237
yboodhan <yboodhan@gmail.com>
2020-04-14 09:47:19 -0700

    Added randomizer and make fetch to one of two urls, 50/50

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ async function handleRequest(request) {
   let response = await fetch('https://cfw-takehome.developers.workers.dev/api/variants');
   let data = await response.json();
 
+  // Create a randomizer (~50%)
+  // Math.random() returns a value from 0 to 1(exclusive)
+  // 50% chance by separating at Math.random() < 0.5
+  // Same as setting index to 1 and changing to 0 about 50% of the time
+  let randomVariantIndex = 1;
+  if (Math.random() < 0.5) {
+    randomVariantIndex = 0;
+  }
+
+  // Request a variant
+  let variant = await fetch(data.variants[randomVariantIndex]);
+
   // Check the data we got
-  return new Response(JSON.stringify(data));
+  return new Response(JSON.stringify(variant));
 }
